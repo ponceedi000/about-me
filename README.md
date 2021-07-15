8dfbce70463b2fea9db6e37326f53f6153d3a950
Ediberto Ponce <ediberto01@gmail.com>
2021-07-14 22:34:41 -0700

    Updated JavaScript and README file

--- README.md
+++ README.md
@@ -2,14 +2,13 @@
 
 ## About Me
 
-I build an app with a direct and powerful purpose. It does all of the things that it accomplishes well. I should describe the purpose and functionality so those that visit my README understand the app
+I created an "About Me" webpage that showcases who I am as a person and a Software Developer. The webpage opens up with a prompt that takes user input followed a quiz that also takes user input utilizing JavaScript. The webpage also provides responses to user input, accomplished with conditional statements.
 
 ### Author: Ediberto Ponce
 
 ### Links and Resources
 * [CSS Tools: Reset CSS](https://meyerweb.com/eric/tools/css/reset/)
-* Any Links you used as reference
-
+* [Git Cheat Sheet](https://www.freecodecamp.org/news/git-cheat-sheet/)
 ### Reflections and Comments
-* Consider including the answers to your daily journal and submission questions here
-* This is also a good place to reflect on the tools and resources used and learned
+* As I began to apply bits and pieces of HTML, CSS, and JS I found that I had to revaluate myself on multiple ocassions. I identified deficiencies and solved them by utilizing resources via open source. I found that this is how I learn best and it works 9/10
+* Initially, I also had some issues with how to utilze Git and Github, but I utilized tools and resources I had at my disposal to guide me through it. I now feel more confident in my abilities to work independently and to problem solve on my own. [freeCodeCamp](https://www.freecodecamp.org/), [MDN Web Docs](https://developer.mozilla.org/en-US/), and [W3 Schools](https://www.w3schools.com/html/) were great resources for me while building this project.
--- js/app.js
+++ js/app.js
@@ -6,15 +6,73 @@ let username = prompt('What\'s your name?');
 alert("Hello " + username);
 
 // ask question 1
-let answerOne = prompt('Do I like Strawberries? Answer \'yes\' or \'no\'').toLowerCase();
+let answerOne = prompt('Do I like olives on my pizza?? Answer \'yes\' or \'no\'').toLowerCase();
 
-//console.log('user ntered to question 1: ' + answerOne);
+//console.log('user entered to question 1: ' + answerOne);
 
 // ask 
 if (answerOne === 'yes' || answerOne === 'y') {
-    alert('You are correct. I like Strawberries.');
+    alert('You are correct. I love olives on my pizza!.');
 } else if (answerOne === 'no' || answerOne === 'n') {
     alert('Sorry, you are wrong.')
 } else {
     alert('Sorry, answers need to be yes or no.')
 }
+
+// ask question 2
+let answerTwo = prompt('Is my favorite color blue? Answer \'yes\' or \'no\'').toLowerCase();
+
+//console.log('user entered to question 2: ' + answerTwo);
+
+// ask 
+if (answerTwo === 'yes' || answerTwo === 'y') {
+    alert('Nope! Red is my favorite color!');
+} else if (answerTwo === 'no' || answerTwo === 'n') {
+    alert('You are correct. I like Strawberries.')
+} else {
+    alert('Sorry, answers need to be yes or no.') 
+}
+
+// ask question 3
+let answerThree = prompt('Have I ever been to Paris? Answer \'yes\' or \'no\'').toLowerCase();
+
+//console.log('user entered to question 3: ' + answerThree);
+
+// ask 
+if (answerThree === 'yes' || answerThree === 'y') {
+    alert('Nope, but I wish!');
+} else if (answerThree === 'no' || answerThree === 'n') {
+    alert('You are correct. I would love to visit one day!')
+} else {
+    alert('Sorry, answers need to be yes or no.')
+}
+
+
+// ask question 4
+let answerFour = prompt('Do I have any tattoos? Answer \'yes\' or \'no\'').toLowerCase();
+
+//console.log('user entered to question 4: ' + answerFour);
+
+// ask 
+if (answerFour === 'yes' || answerFour=== 'y') {
+    alert('Correct! I have 3 so far!');
+} else if (answerFour === 'no' || answerFour === 'n') {
+    alert('Incorrect, I have 3 at the moment.')
+} else {
+    alert('Sorry, answers need to be yes or no.')
+}
+
+// ask question 5
+let answerFive = prompt('Am I muscian? Answer \'yes\' or \'no\'').toLowerCase();
+
+//console.log('user entered to question 5: ' + answerFive);
+
+// ask 
+if (answerFive === 'yes' || answerFive === 'y') {
+    alert('You are correct. I play 5 different instruments!');
+} else if (answerFive === 'no' || answerFive === 'n') {
+    alert('Sorry, you are wrong.')
+} else {
+    alert('Sorry, answers need to be yes or no.')
+}
+
