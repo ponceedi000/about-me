ebcd6be1c065c47541145f5bca0b7203fbf5086b
Ediberto Ponce <ediberto01@gmail.com>
2021-07-17 18:53:14 -0700

    Added another loop with a nested array and added more bullet points to HTML

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ let score = 0;
 // get the user's name so we can greet them
 let username = prompt('What\'s your name?');
 // greet the user
@@ -12,7 +12,7 @@ let answerOne = prompt('Do I like olives on my pizza?? Answer \'yes\' or \'no\''
 
 // create response to answers
 if (answerOne === 'yes' || answerOne === 'y') {
-    alert('You are correct. I love olives on my pizza!.');
+    alert('You are correct. I love olives on my pizza!.' + score++);
 } else if (answerOne === 'no' || answerOne === 'n') {
     alert('Sorry, you are wrong.')
 } else {
@@ -28,7 +28,7 @@ let answerTwo = prompt('Is my favorite color blue? Answer \'yes\' or \'no\'').to
 if (answerTwo === 'yes' || answerTwo === 'y') {
     alert('Nope! Red is my favorite color!');
 } else if (answerTwo === 'no' || answerTwo === 'n') {
-    alert('You are correct. My favorite color is actually red.')
+    alert('You are correct. My favorite color is actually red.' + score++)
 } else {
     alert('Sorry, answers need to be yes or no.') 
 }
@@ -42,7 +42,7 @@ let answerThree = prompt('Have I ever been to Paris? Answer \'yes\' or \'no\'').
 if (answerThree === 'yes' || answerThree === 'y') {
     alert('Nope, but I wish!');
 } else if (answerThree === 'no' || answerThree === 'n') {
-    alert('Correct. I would love to visit one day however!')
+    alert('Correct. I would love to visit one day however!' + score++)
 } else {
     alert('Sorry, answers need to be yes or no.')
 }
@@ -55,7 +55,7 @@ let answerFour = prompt('Do I have any tattoos? Answer \'yes\' or \'no\'').toLow
 
 // create response to answers
 if (answerFour === 'yes' || answerFour=== 'y') {
-    alert('Correct! I have 3 so far!');
+    alert('Correct! I have 3 so far!' + score++);
 } else if (answerFour === 'no' || answerFour === 'n') {
     alert('Incorrect, I actually have 3 at the moment.')
 } else {
@@ -69,24 +69,17 @@ let answerFive = prompt('Am I muscian? Answer \'yes\' or \'no\'').toLowerCase();
 
 // create response to answers
 if (answerFive === 'yes' || answerFive === 'y') {
-    alert('You are correct. I play 5 different instruments!');
+    alert('You are correct. I play 5 different instruments!' + score++);
 } else if (answerFive === 'no' || answerFive === 'n') {
     alert('Sorry, I\'m definitely a musician.')
 } else {
     alert('Sorry, answers need to be yes or no.')
 }
 
- alert('Thank you for playing, ' + username + '! Make sure to visit my webpage to learn more about me :)')
-
-
-// SUDO CODE
-// comments that describe what I need to do.
-// step by step
-
 // Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
-//Indicates through an alert if the guess is “too high” or “too low”.
-//It should give the user exactly four opportunities to get the correct answer.
-//After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+// Indicates through an alert if the guess is “too high” or “too low”.
+// It should give the user exactly four opportunities to get the correct answer.
+// After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
 
 // Add 6th question
 // respond if too high
@@ -103,8 +96,8 @@ while (userResponse !== myAge) {
     userResponse = +prompt('How old am I? Please enter a numerical value only');
     console.log(`Guess of my age: ${userResponse}`);
     if (userResponse === myAge) {
-        alert('You are correct. Nice job.');
-        // break;
+        alert('You are correct. Nice job.' + score++);
+        break;
     }
 
     if (userResponse < myAge) {
@@ -115,11 +108,35 @@ while (userResponse !== myAge) {
         alert(`Too high! you have ${attemptsRemaining} attempts remaining.`);
     }
     attemptsRemaining--;
-    // if(attemptsRemaining === 0 && !myAge) {
-    // alert('Sorry, the correct answer is ' + myAge)
-    // } else if(userResponse === myAge)
-    // console.log('You are right!')
+    if(attemptsRemaining === 0 && userResponse !== myAge) {
+    alert('Sorry, the correct answer is ' + myAge)
+    } else if(userResponse === myAge)
+    alert('You are right!' + score++)
     console.log('Attempts remaining', attemptsRemaining)
 }
 
 
+let myFavoriteAnimals = ['wolf', 'tiger', 'gorilla', 'eagle', 'bear' ]
+let userInput = prompt ('what is my favorite animal?');
+let attempts = 5;
+let guessedCorrectly = false;
+
+while (attempts) {
+    if (!attempts) {
+        alert('You are out of attempts');
+        break;
+    }
+    for (let i = 0; i < myFavoriteAnimals.length; i++) {
+        if (userInput === myFavoriteAnimals[i]) {
+            alert('Nice! You got it!' + score++)
+        }
+    }
+if (guessedCorrectly) {
+     attempts = 0;
+    break;
+}
+alert(`Please try again, you have ${attempts} remaining`);
+userInput = prompt('What is my favorite animal?');
+attempts--;
+}
+alert(`Good job, you got ${score} out of 7!`)
